3e4d42413ff9ac939856b3da0c1c941808441894
Sandeep Sharma <sandeepsharma2183@gmail.com>
2023-12-16 15:14:44 +0530

    scroll indicator added

--- script.js
+++ script.js
@@ -85,3 +85,13 @@ function copyLink(link) {
     var confirmationMessage = document.getElementById('confirmation-message');
     confirmationMessage.classList.remove('hidden');
   }
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
